e6dc56fdd6796b0a1dfc0139955c0dc190963f10
Oliveira <oliveirmic@paho.org>
2023-07-25 09:52:06 -0300

    Ajuste KeyWords e Journal

--- src/main/scala/sp/Journal.scala
+++ src/main/scala/sp/Journal.scala
@@ -4,12 +4,6 @@ import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.udf
 
 object Journal {
-  def fix(in: String): String = {
-    if (in.startsWith("\"") && in.endsWith("\"") || in.startsWith("\"") || in.endsWith("\""))
-      GeneralFix.fix(in.substring(1, in.length - 1))
-    else
-      GeneralFix.fix(in)
-
-  }
+  def fix(in: String): String = GeneralFix.fix(in)
   val _udf: UserDefinedFunction = udf((in: String) => fix(in))
 }
--- src/main/scala/sp/Keywords.scala
+++ src/main/scala/sp/Keywords.scala
@@ -4,11 +4,6 @@ import org.apache.spark.sql.expressions.UserDefinedFunction
 import org.apache.spark.sql.functions.udf
 
 object Keywords {
-  def fix(in: String): String = {
-    if (in.startsWith("\"") && in.endsWith("\""))
-      GeneralFix.fix(in.substring(1, in.length - 1).replaceAll("\"\"", "\""))
-    else
-      GeneralFix.fix(in)
-  }
+  def fix(in: String): String = GeneralFix.fix(in)
   val _udf: UserDefinedFunction = udf((in: String) => fix(in))
 }
